462f5671b584ca71feb0a319338975887275d2d5
Millzy <millzyyt08@gmail.com>
2022-01-30 13:18:50 +0000

    switch to vector<pair<>>

--- include/Parsing.hpp
+++ include/Parsing.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
-#include <map>
+#include <vector>
+#include <utility>
 #include <string>
 
 #include "beatsaber-hook/shared/rapidjson/include/rapidjson/memorystream.h"
@@ -8,7 +9,7 @@
 namespace Diglett {
     class Parsing {
     public:
-        static std::map<std::string, std::string> ParseJson(rapidjson::MemoryStream memoryStream);
+        static std::vector<std::pair<std::string, std::string>> ParseJson(rapidjson::MemoryStream memoryStream);
         //std::map<std::string, std::string> ParseXml(std::string xml);
     };
 }
--- src/Parsing.cpp
+++ src/Parsing.cpp
@@ -8,15 +8,15 @@ using namespace rapidjson;
 
 #include <utility>
 
-std::map<std::string, std::string> Parsing::ParseJson(rapidjson::MemoryStream memoryStream) {
+std::vector<std::pair<std::string, std::string>> Parsing::ParseJson(rapidjson::MemoryStream memoryStream) {
     Document document;
     document.ParseStream(memoryStream);
 
-    auto map = std::map<std::string, std::string>();
+    auto map = std::vector<std::pair<std::string, std::string>>();
 
     for (auto &obj : document.GetObject()) {
         getLogger().info("Adding locale to map - %s: %s", obj.name.GetString(), obj.value.GetString());
-        map.insert(std::pair<std::string, std::string>(obj.name.GetString(), obj.value.GetString()));
+        map.emplace_back(obj.name.GetString(), obj.value.GetString());
     }
 
     return map;
