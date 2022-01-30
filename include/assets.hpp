f4d5e54922e1b3dc800f1ab279fedcba9bcceebf
Millzy <millzyyt08@gmail.com>
2022-01-30 10:09:13 +0000

    include main

--- include/assets.hpp
+++ include/assets.hpp
@@ -1,5 +1,4 @@
-#include "main.hpp"
-
+#include"main.hpp"
 #ifdef TESTS
 #define DECLARE_FILE(name, prefix) extern "C" uint8_t _binary_##name##_start[]; extern "C" uint8_t _binary_##name##_end[]; struct prefix##name { static size_t getLength() { return _binary_##name##_end - _binary_##name##_start; } static uint8_t* getData() { return _binary_##name##_start; } };
 DECLARE_FILE(de_json,)
