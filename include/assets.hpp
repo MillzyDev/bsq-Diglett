d24613c9698e71b267cc06e1d166e2816ade0880
Millzy <millzyyt08@gmail.com>
2022-01-30 11:54:10 +0000

    removed this- dont need it

--- include/assets.hpp
+++ include/assets.hpp
@@ -1,5 +1,3 @@
-#include"main.hpp"
-#ifdef TESTS
 #define DECLARE_FILE(name, prefix) extern "C" uint8_t _binary_##name##_start[]; extern "C" uint8_t _binary_##name##_end[]; struct prefix##name { static size_t getLength() { return _binary_##name##_end - _binary_##name##_start; } static uint8_t* getData() { return _binary_##name##_start; } };
 DECLARE_FILE(de_json,)
 DECLARE_FILE(en_json,)
@@ -7,4 +5,3 @@ DECLARE_FILE(es_json,)
 DECLARE_FILE(fr_json,)
 DECLARE_FILE(ja_json,)
 DECLARE_FILE(ko_json,)
-#endif
