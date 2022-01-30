ac4308c290ff6480f0b9f2ef93a9069070c7205a
Millzy <millzyyt08@gmail.com>
2022-01-30 11:55:33 +0000

    new macro

--- shared/Register.hpp
+++ shared/Register.hpp
@@ -11,13 +11,21 @@
 /**
  * Used to convert assets from " laurie's fully™️-functional objcopy cmake script" to rapidjson::MemoryStreams without too much eyesore
  */
-#define ASSET_TO_JSON(asset) rapidjson::MemoryStream(reinterpret_cast<char *>(asset::getData), asset::getLength())
+#define ASSET_TO_JSON(asset) rapidjson::MemoryStream(copyStr(asset::getData(), asset::getLength()), asset::getLength())
 
+inline char *copyStr(uint8_t *data, size_t length) {
+    char* str = new char[length + 1];
+    memcpy(str, data, length);
+    str[length] = '\0';
+    return str;
+}
 
 namespace Diglett {
     class Register {
 
     public:
+
+
         /**
          * Register locale json files
          * @tparam L The language of the inputted file
@@ -25,9 +33,9 @@ namespace Diglett {
          */
         template<Languages L>
         static void RegisterLocales(rapidjson::MemoryStream memoryStream) {
+            getLogger().info("Registering JSON locale");
             LocalizationDocument ld;
             switch (L) {
-                default:
                 case Languages::EN:
                     ld = LocalizationDocument::GetEN();
                     break;
