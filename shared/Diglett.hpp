3570dfe40b433cd9aeeba6439214cdb88c49d163
Millzy <millzyyt08@gmail.com>
2022-01-30 09:22:11 +0000

    Create Diglett.hpp

---
+++ shared/Diglett.hpp
@@ -0,0 +1,6 @@
+#include "Format.hpp"
+#include "Languages.hpp"
+#include "LocalizationDocument.hpp"
+#include "Register.hpp"
+
+#include "beatsaber-hook/shared/rapidjson/include/rapidjson/memorystream.h"
