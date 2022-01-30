dd8dada1073a3939b93b4bd966e665801c7e31be
Millzy <millzyyt08@gmail.com>
2022-01-30 23:36:52 +0000

    namespace it

--- shared/Languages.hpp
+++ shared/Languages.hpp
@@ -1,10 +1,12 @@
 #pragma once
 
-enum Languages {
-    English = 0,
-    French = 1,
-    Spanish = 2,
-    German = 3,
-    Japanese = 16,
-    Korean = 19
-};
+namespace Diglett {
+    enum Languages {
+        English = 0,
+        French = 1,
+        Spanish = 2,
+        German = 3,
+        Japanese = 16,
+        Korean = 19
+    };
+}
