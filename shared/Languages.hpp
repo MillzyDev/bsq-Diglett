f7f0680035d5cc90a88e7a953f2d3e09e6d3398c
Millzy <millzyyt08@gmail.com>
2022-01-30 20:01:48 +0000

    made this better

--- shared/Languages.hpp
+++ shared/Languages.hpp
@@ -1,12 +1,10 @@
 #pragma once
 
-namespace Diglett {
-    enum Languages {
-        EN,
-        FR,
-        ES,
-        DE,
-        JA,
-        KO
-    };
-}
+enum Languages {
+    English = 0,
+    French = 1,
+    Spanish = 2,
+    German = 3,
+    Japanese = 16,
+    Korean = 19
+};
