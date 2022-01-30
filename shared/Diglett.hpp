4992e89d01ed891c637fd75ec305f2e609644d07
Millzy <millzyyt08@gmail.com>
2022-01-30 20:01:55 +0000

    qol stuff

--- shared/Diglett.hpp
+++ shared/Diglett.hpp
@@ -1,6 +1,9 @@
 #include "Format.hpp"
 #include "Languages.hpp"
 #include "LocalizationDocument.hpp"
+#include "Parsing.hpp"
 #include "Register.hpp"
 
 #include "beatsaber-hook/shared/rapidjson/include/rapidjson/memorystream.h"
+
+typedef Diglett::LocalizationDocument Localization;
