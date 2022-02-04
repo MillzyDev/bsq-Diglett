de1e8fe3beadd419096a08e5f15863c1d7fe883e
Millzy <millzyyt08@gmail.com>
2022-02-04 18:55:45 +0000

    yeeted rapidjson

--- shared/Parsing.hpp
+++ shared/Parsing.hpp
@@ -4,19 +4,13 @@
 #include <utility>
 #include <string>
 
-#include "beatsaber-hook/shared/config/rapidjson-utils.hpp"
-#include "beatsaber-hook/shared/rapidjson/include/rapidjson/encodedstream.h"
-#include "beatsaber-hook/shared/rapidjson/include/rapidjson/document.h"
-#include "beatsaber-hook/shared/rapidjson/include/rapidjson/encodings.h"
+#include "nlohmann/json.hpp"
 //#include "rapidxml/rapidxml.hpp"
-
-#include "main.hpp"
 
 namespace Diglett {
     class Parsing {
     public:
-        static std::map<std::string, std::u16string> ParseJson(
-                rapidjson::EncodedInputStream<rapidjson::UTF16<char16_t>, rapidjson::MemoryStream>& memoryStream);
+        static std::map<std::string, std::u16string> ParseJson(const std::u16string& json);
         //static std::map<std::string, std::u16string> ParseXml(char16_t *xml);
     };
 }
