2e6e7301b5eef4f816d709226404b99a0bfce32d
Millzy <millzyyt08@gmail.com>
2022-02-12 12:27:52 +0000

    fixed some issues with languages

--- shared/Languages.hpp
+++ shared/Languages.hpp
@@ -1,14 +1,13 @@
 #pragma once
 
 namespace Diglett {
-    class Languages {
-        enum {
-            English = 0,
-            French = 1,
-            Spanish = 2,
-            German = 3,
-            Japanese = 16,
-            Korean = 19
-        };
+    enum class Language {
+        English = 0,
+        French = 1,
+        Spanish = 2,
+        German = 3,
+        Japanese = 16,
+        Korean = 19
     };
 }
+
