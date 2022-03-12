fd5a5401a76e4120f8f5bddbbbf69c15fddee119
Millzy <millzyyt08@gmail.com>
2022-03-12 09:00:27 +0000

    removed irrelevant files

--- shared/Diglett.hpp
+++
@@ -1,8 +0,0 @@
-#include "Languages.hpp"
-#include "LocalizationDocument.hpp"
-#include "Parsing.hpp"
-#include "Register.hpp"
-
-typedef Diglett::LocalizationDocument Localization;
-typedef Diglett::Register LocaleRegister;
-typedef Diglett::Language Language;
