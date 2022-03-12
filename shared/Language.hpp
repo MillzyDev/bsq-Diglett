2d0e06c5972d941d1eeac32239314be85d1567b1
Millzy <millzyyt08@gmail.com>
2022-03-12 09:03:10 +0000

    language enum

---
+++ shared/Language.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace Diglett {
+    enum Language {
+        ENGLISH = 0,
+        FRENCH = 1,
+        SPANISH = 2,
+        GERMAN = 3,
+        JAPANESE = 16,
+        KOREAN = 19
+    };
+}
