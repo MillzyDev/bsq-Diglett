ec4d63a5afbe1f175d8b640d90a67b6279d008a9
Millzy <millzyyt08@gmail.com>
2022-03-15 20:37:28 +0000

    debug stuff

--- src/Util.cpp
+++ src/Util.cpp
@@ -1,7 +1,10 @@
 #include "Util.hpp"
+#include "main.hpp"
+#include "Localization.hpp"
 
 #include "Polyglot/Localization.hpp"
 
 Diglett::Language Diglett::getSelectedLanguage() {
+    getLogger().info("Selected language is NO %i", Polyglot::Localization::get_Instance()->get_SelectedLanguage().value);
     return (Diglett::Language) Polyglot::Localization::get_Instance()->get_SelectedLanguage().value;
 }
