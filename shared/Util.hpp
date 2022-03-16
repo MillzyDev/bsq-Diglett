6e9ff007ce6f24223171f9fff8af00b61eb5876d
Millzy <millzyyt08@gmail.com>
2022-03-16 22:48:53 +0000

    component method

--- shared/Util.hpp
+++ shared/Util.hpp
@@ -2,6 +2,15 @@
 
 #include "Language.hpp"
 
+#if __has_include("questui/shared/BeatSaberUI.hpp")
+#include "TMPro/TextMeshProUGUI.hpp"
+#endif
+
 namespace Diglett {
     Language getSelectedLanguage();
+
+#if __has_include("questui/shared/BeatSaberUI.hpp")
+    [[maybe_unused]]
+    TMPro::TextMeshProUGUI *CreateLocalizedText(UnityEngine::Transform *parent, std::string key, bool italic = true);
+#endif
 }
--- src/Util.cpp
+++ src/Util.cpp
@@ -1,10 +1,23 @@
 #include "Util.hpp"
+
+#include <utility>
 #include "main.hpp"
 #include "Localization.hpp"
 
 #include "Polyglot/Localization.hpp"
 
+#include "questui/shared/BeatSaberUI.hpp"
+#include "Components/LocalizedText.hpp"
+using namespace Diglett::Components;
+
 Diglett::Language Diglett::getSelectedLanguage() {
     getLogger().info("Selected language is NO %i", Polyglot::Localization::get_Instance()->get_SelectedLanguage().value);
     return (Diglett::Language) Polyglot::Localization::get_Instance()->get_SelectedLanguage().value;
+}
+
+TMPro::TextMeshProUGUI *Diglett::CreateLocalizedText(UnityEngine::Transform *parent, std::string key, bool italic) {
+    auto text = QuestUI::BeatSaberUI::CreateText(parent, "", italic);
+    auto localizedText = text->get_gameObject()->AddComponent<LocalizedText *>();
+    localizedText->set_key(std::move(key));
+    return text;
 }
