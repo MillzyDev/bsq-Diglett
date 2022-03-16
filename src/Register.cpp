8e9c112f710aad52124780d9fee50b7e3f1d3aa4
Millzy <millzyyt08@gmail.com>
2022-03-16 06:37:53 +0000

    manually insert

--- src/Register.cpp
+++ src/Register.cpp
@@ -6,6 +6,9 @@
 void Diglett::RegisterAsset(Diglett::LocalizationAsset asset, Diglett::Language language) {
     auto localization = Localization::get_instance();
     auto localizations = asset.get_localizations();
-    getLogger().info("Inserting Locales");
-    localization->getLocales(language).insert(localizations.begin(), localizations.end());
+    auto &locales = localization->getLocales(language);
+    for (const auto& p : localizations) {
+        locales.insert(p);
+        getLogger().info("Registered %s - %s", p.first.c_str(), p.second.data());
+    }
 }
