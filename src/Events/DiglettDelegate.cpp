e4e2ba1ff5c02f295448331ccf454d96d0459c35
Millzy <millzyyt08@gmail.com>
2022-03-17 07:59:14 +0000

    yeet that shit

--- src/Events/DiglettDelegate.cpp
+++
@@ -1,9 +0,0 @@
-#include "Events/DiglettDelegate.hpp"
-
-void Diglett::Events::DiglettDelegate::Invoke() {
-    delegate();
-}
-
-Diglett::Events::DiglettDelegate::DiglettDelegate(std::function<void()> callback) {
-    delegate = std::move(callback);
-}
