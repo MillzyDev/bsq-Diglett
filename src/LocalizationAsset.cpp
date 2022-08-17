5039bc6477791da3b4c42b6b46dee3024436b461
Millzy <millzyyt08@gmail.com>
2022-08-17 12:58:41 +0100

    newline support

--- src/LocalizationAsset.cpp
+++ src/LocalizationAsset.cpp
@@ -1,10 +1,13 @@
 #include "LocalizationAsset.hpp"
 #include "main.hpp"
 
+#include <string>
+
 #include "tinyxml2/shared/tinyxml2.h"
 
 #define RESOURCES_NODE "resources"
 #define STRING_NODE "string"
+#define NEW_LINE "\\n"
 
 void Diglett::LocalizationAsset::set_text(std::string_view text) {
     this->text = text;
@@ -26,7 +29,17 @@ Diglett::LocalizationAsset::LocalizationAsset(std::string_view str) {
 
     auto resources = doc.FirstChildElement(RESOURCES_NODE);
 
+    std::string newLine = NEW_LINE;
+
     for (auto string = resources->FirstChildElement(STRING_NODE); string != nullptr; string = string->NextSiblingElement(STRING_NODE)) {
+        std::string content = string->GetText();
+
+        size_t pos = content.find(newLine);
+        while (pos != std::string::npos) {
+            content.replace(pos, newLine.size(), "\n");
+            pos = content.find(newLine, pos + std::string("\n").size());
+        }
+
         localizations.emplace(
                 std::string(string->FindAttribute("name")->Value()),
                 std::string_view(string->GetText())
